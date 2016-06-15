86b90fce3986bda0266d10398d6af482039e754a
Bailey <bailey@rcbailey.com>
2016-06-15 14:20:17 +0200

    remove outdated comments

--- cppcryptfs/cryptio.cpp
+++ cppcryptfs/cryptio.cpp
@@ -68,13 +68,6 @@ read_block(CryptContext *con, HANDLE hfile, const unsigned char *fileid, unsigne
 
 	if (nread == 0)
 		return 0;
-		
-	/*
-	int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *aad,
-	int aad_len, unsigned char *tag, unsigned char *key, unsigned char *iv, int ivlen,
-	unsigned char *plaintext, int mode)
-	*/
-
 
 	int ptlen = decrypt(buf + BLOCK_IV_LEN, nread - BLOCK_IV_LEN - BLOCK_TAG_LEN, auth_data, sizeof(auth_data), 
 		buf + nread - BLOCK_TAG_LEN, con->GetConfig()->GetKey(), buf, ptbuf, crypt_context);
@@ -114,13 +107,6 @@ write_block(CryptContext *con, HANDLE hfile, const unsigned char *fileid, unsign
 	memcpy(auth_data + sizeof(be_block), fileid, FILE_ID_LEN);
 
 	unsigned char buf[CIPHER_BS];
-
-
-	/*
-	int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *aad,
-	int aad_len, unsigned char *key, unsigned char *iv, int ivlen,
-	unsigned char *ciphertext, unsigned char *tag, int mode)
-	*/
 
 	unsigned char tag[BLOCK_TAG_LEN];
 
