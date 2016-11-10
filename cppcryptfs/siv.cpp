23c56695401aaa9489ab847e80c98e8aed6e4218
Bailey <bailey.brown@gmail.com>
2016-11-10 16:56:59 +0100

    use lock zero buffer instead of stack buffer for briefly holding derived 512bit aes-siv key

--- cppcryptfs/siv.cpp
+++ cppcryptfs/siv.cpp
@@ -24,15 +24,15 @@ bool SivContext::SetKey(const unsigned char *key, int keylen)
 	if (!m_pKeys)
 		m_pKeys = new LockZeroBuffer<AES_KEY>(4, true);
 
-	unsigned char key64[64];
+	LockZeroBuffer<BYTE> key64(64, true);
 
-	if (!sha512(key, 32, key64))
+	if (!sha512(key, 32, key64.m_buf))
 		return false;
 
-	AES::initialize_keys(key64, 256, &m_pKeys->m_buf[SIV_KEY_ENCRYPT_LOW_INDEX], 
+	AES::initialize_keys(key64.m_buf, 256, &m_pKeys->m_buf[SIV_KEY_ENCRYPT_LOW_INDEX], 
 										&m_pKeys->m_buf[SIV_KEY_DECRYPT_LOW_INDEX]);
 
-	AES::initialize_keys(key64 + 32 , 256, &m_pKeys->m_buf[SIV_KEY_ENCRYPT_HIGH_INDEX], 
+	AES::initialize_keys(key64.m_buf + 32 , 256, &m_pKeys->m_buf[SIV_KEY_ENCRYPT_HIGH_INDEX], 
 											&m_pKeys->m_buf[SIV_KEY_DECRYPT_HIGH_INDEX]);
 
 	return true;
