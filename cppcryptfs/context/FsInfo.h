3a316ef88d24840c10ba06d42ee13f54a01f5547
Bailey <bailey.brown@gmail.com>
2018-03-04 10:20:33 -0500

    add copyright to new files. move FsInfo to own file.

---
+++ cppcryptfs/context/FsInfo.h
@@ -0,0 +1,54 @@
+/*
+cppcryptfs : user-mode cryptographic virtual overlay filesystem.
+
+Copyright (C) 2016-2018 Bailey Brown (github.com/bailey27/cppcryptfs)
+
+cppcryptfs is based on the design of gocryptfs (github.com/rfjakob/gocryptfs)
+
+The MIT License (MIT)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+#pragma once
+
+#include <string>
+
+using namespace std;
+
+class FsInfo {
+public:
+	wstring path;
+	wstring configPath;
+	wstring fileNameEncryption;
+	wstring dataEncryption;
+	float dirIvCacheHitRatio;
+	float lfnCacheHitRatio;
+	float caseCacheHitRatio;
+	int ioBufferSize;
+	int fsThreads;
+	int cacheTTL;
+	bool readOnly;
+	bool reverse;
+	bool mountManager;
+	bool caseInsensitive;
+
+	FsInfo();
+	virtual ~FsInfo();
+};
+
