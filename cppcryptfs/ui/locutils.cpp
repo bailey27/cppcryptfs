58f73871371dd8937e82f9ac47bc268c203df36a
nbb1967 <nbb1967@yandex.ru>
2026-02-06 21:23:14 +0300

    debugging

--- cppcryptfs/ui/locutils.cpp
+++ cppcryptfs/ui/locutils.cpp
@@ -3,7 +3,8 @@
 
 CString LocUtils::GetStringFromResources(UINT nID) {
     CString str;
-    if (!str.LoadString(AfxGetResourceHandle(), nID)) {
+    static HINSTANCE hExe = GetModuleHandle(NULL);
+    if (hExe == NULL || !str.LoadString(hExe, nID)) {
         return _T("Error: String not found");
     }
     return str;
